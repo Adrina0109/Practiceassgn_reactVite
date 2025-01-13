25e0a8e027c2b6c92030e4bd3d75302761615386
Adrina bernadin <mary.adrina@kalvium.community>
2025-01-13 14:26:28 +0530

    initial commit

---
+++ practiceassgn/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <header className='title' >
+        <h1>MARY ADRINA BERNADIN</h1>
+        <p>CONTACT: 9003260101</p>
+      </header>
+    </div>
+  )
+}
+
+export default App;
