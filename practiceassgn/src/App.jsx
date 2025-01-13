24d67f5897632f8d3ef892b3fbb63b7706660eb6
Adrina bernadin <mary.adrina@kalvium.community>
2025-01-13 14:27:57 +0530

    made changes

--- practiceassgn/src/App.jsx
+++ practiceassgn/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <header className='title' >
         <h1>MARY ADRINA BERNADIN</h1>
         <p>CONTACT: 9003260101</p>
+        <p>Email: adz@gmail.com</p>
       </header>
     </div>
   )
